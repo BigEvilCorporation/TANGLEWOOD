f9666d49f93cf9b7d4b1f9037441fdb46900304c
Matt <mattphillips@mail.com>
2016-11-07 06:58:43 +0000

    Tanglewood: Kickstarter logo screen

---
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/KS_LOGO.asm
@@ -0,0 +1,14 @@
+; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
+;   Created with BEEhive - the complete art tool for SEGA Mega Drive
+; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
+;   http://www.bigevilcorporation.co.uk
+; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
+;   BEEhive and SEGA Genesis Framework (c) Matt Phillips 2015
+; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
+
+
+map_ks_logo_size_b	equ 0x00000960	; Size in bytes
+map_ks_logo_size_w	equ (map_ks_logo_size_b/2)	; Size in words
+map_ks_logo_size_l	equ (map_ks_logo_size_b/4)	; Size in longwords
+map_ks_logo_width	equ 0x28
+map_ks_logo_height	equ 0x1E
