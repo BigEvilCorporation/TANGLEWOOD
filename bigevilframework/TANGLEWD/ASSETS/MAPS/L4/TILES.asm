287ef34d5f1a2cf3d6030cacbd3cd4e77b9e3b31
Matt <mattphillips@mail.com>
2017-03-04 22:40:20 +0000

    Tanglewood: Level 4 - Tethered - Initial commit

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L4/TILES.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L4/TILES.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-TerrainTiles_L3_size_b	equ 0x00000398	; Size in bytes
-TerrainTiles_L3_size_w	equ (TerrainTiles_L3_size_b/2)	; Size in words
-TerrainTiles_L3_size_l	equ (TerrainTiles_L3_size_b/4)	; Size in longwords
-TerrainTiles_L3_size_t	equ (TerrainTiles_L3_size_b/32)	; Size in tiles
+tiles_L4_size_b	equ 0x00004900	; Size in bytes
+tiles_L4_size_w	equ (tiles_L4_size_b/2)	; Size in words
+tiles_L4_size_l	equ (tiles_L4_size_b/4)	; Size in longwords
+tiles_L4_size_t	equ (tiles_L4_size_b/32)	; Size in tiles
