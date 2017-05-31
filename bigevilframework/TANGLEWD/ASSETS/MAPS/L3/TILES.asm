adc8085be565d2e4da6d173703bd878bc0fd1e8c
Matt <mattphillips@mail.com>
2017-05-31 01:16:34 +0100

    Tanglewood: L3A1 counterweight puzzle

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L3/TILES.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L3/TILES.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-tiles_L3_size_b	equ 0x00004DC0	; Size in bytes
+tiles_L3_size_b	equ 0x00004F80	; Size in bytes
 tiles_L3_size_w	equ (tiles_L3_size_b/2)	; Size in words
 tiles_L3_size_l	equ (tiles_L3_size_b/4)	; Size in longwords
 tiles_L3_size_t	equ (tiles_L3_size_b/32)	; Size in tiles
