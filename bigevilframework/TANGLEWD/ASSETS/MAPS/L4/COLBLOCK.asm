6760e2f82da132fa4da3c39999e48f071a412f8b
Matt <mattphillips@mail.com>
2017-10-05 20:07:54 +0100

    Tanglewood: L5A4 storm and entrance lift

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L4/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L4/COLBLOCK.asm
@@ -7,6 +7,6 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L4_size_b	equ 0x00005120	; Size in bytes
+terrainmap_blocks_L4_size_b	equ 0x000054E0	; Size in bytes
 terrainmap_blocks_L4_size_w	equ (terrainmap_blocks_L4_size_b/2)	; Size in words
 terrainmap_blocks_L4_size_l	equ (terrainmap_blocks_L4_size_b/4)	; Size in longwords
