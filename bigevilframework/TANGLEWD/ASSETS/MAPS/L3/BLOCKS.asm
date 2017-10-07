80b5dffe1a4bf923401c09a1719d6f43bfcbc403
Matt <mattphillips@mail.com>
2017-10-07 13:44:06 +0100

    Tanglewood: Level size crop pass

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L3/BLOCKS.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L3/BLOCKS.asm
@@ -7,6 +7,6 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-map_blocks_L3_size_b	equ 0x00025560	; Size in bytes
+map_blocks_L3_size_b	equ 0x00025580	; Size in bytes
 map_blocks_L3_size_w	equ (map_blocks_L3_size_b/2)	; Size in words
 map_blocks_L3_size_l	equ (map_blocks_L3_size_b/4)	; Size in longwords
