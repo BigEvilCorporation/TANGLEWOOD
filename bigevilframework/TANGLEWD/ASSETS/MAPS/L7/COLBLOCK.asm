1a366a5830d19bc7c04997456fdc40c1bff8e309
Matt <mattphillips@mail.com>
2017-10-08 12:32:25 +0100

    Tanglewood: L8A1 initial commit

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L7/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L7/COLBLOCK.asm
@@ -7,6 +7,6 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L7_size_b	equ 0x0000D8C0	; Size in bytes
+terrainmap_blocks_L7_size_b	equ 0x0000ED40	; Size in bytes
 terrainmap_blocks_L7_size_w	equ (terrainmap_blocks_L7_size_b/2)	; Size in words
 terrainmap_blocks_L7_size_l	equ (terrainmap_blocks_L7_size_b/4)	; Size in longwords
