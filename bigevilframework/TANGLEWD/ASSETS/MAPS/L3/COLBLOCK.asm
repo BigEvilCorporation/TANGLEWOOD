76a0e4e49d05cd8968932b16b7453a63f0c0df96
Matt <mattphillips@mail.com>
2017-10-12 19:54:28 +0100

    Tanglewood: Fixed L3A3 Djakk falling through trap area

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L3/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L3/COLBLOCK.asm
@@ -7,6 +7,6 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L3_size_b	equ 0x00003700	; Size in bytes
+terrainmap_blocks_L3_size_b	equ 0x000036C0	; Size in bytes
 terrainmap_blocks_L3_size_w	equ (terrainmap_blocks_L3_size_b/2)	; Size in words
 terrainmap_blocks_L3_size_l	equ (terrainmap_blocks_L3_size_b/4)	; Size in longwords
