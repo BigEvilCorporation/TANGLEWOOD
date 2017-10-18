284e4a8a5962fbc91664338b53e841ed3a13e09c
Matt <mattphillips@mail.com>
2017-10-18 01:14:05 +0100

    Tanglewood: L5A3 updates

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L5/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L5/COLBLOCK.asm
@@ -7,6 +7,6 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L5_size_b	equ 0x00004220	; Size in bytes
+terrainmap_blocks_L5_size_b	equ 0x00004040	; Size in bytes
 terrainmap_blocks_L5_size_w	equ (terrainmap_blocks_L5_size_b/2)	; Size in words
 terrainmap_blocks_L5_size_l	equ (terrainmap_blocks_L5_size_b/4)	; Size in longwords
