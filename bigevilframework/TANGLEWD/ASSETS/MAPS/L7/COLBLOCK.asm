0c8f2da62047229888b6719640f4830debe289cc
Matt <mattphillips@mail.com>
2017-10-22 01:28:21 +0100

    Tanglewood: L7 collision re-export

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L7/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L7/COLBLOCK.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L7_size_b	equ 0x0000E360	; Size in bytes
+terrainmap_blocks_L7_size_b	equ 0x0000EA60	; Size in bytes
 terrainmap_blocks_L7_size_w	equ (terrainmap_blocks_L7_size_b/2)	; Size in words
 terrainmap_blocks_L7_size_l	equ (terrainmap_blocks_L7_size_b/4)	; Size in longwords
-terrainmap_blocks_L7_num_blocks	equ 1819	; Size in blocks
+terrainmap_blocks_L7_num_blocks	equ 1875	; Size in blocks
