8465f6f599fcbbfba97d4c0cc8330fca0689f0af
Matt <mattphillips@mail.com>
2017-10-26 14:14:51 +0100

    Tanglewood: L8A2 updates

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L7/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L7/COLBLOCK.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L7_size_b	equ 0x0000F060	; Size in bytes
+terrainmap_blocks_L7_size_b	equ 0x0000F080	; Size in bytes
 terrainmap_blocks_L7_size_w	equ (terrainmap_blocks_L7_size_b/2)	; Size in words
 terrainmap_blocks_L7_size_l	equ (terrainmap_blocks_L7_size_b/4)	; Size in longwords
-terrainmap_blocks_L7_num_blocks	equ 1923	; Size in blocks
+terrainmap_blocks_L7_num_blocks	equ 1924	; Size in blocks
