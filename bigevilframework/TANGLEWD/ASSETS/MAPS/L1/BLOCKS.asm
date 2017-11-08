0ac90bf222364778b6aae80e4e52f8d5ceb3450f
Matt <mattphillips@mail.com>
2017-11-08 22:18:02 +0000

    Tanglewood: Adjusted mushroom bounce velocity to max 8 pixels/sec

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L1/BLOCKS.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L1/BLOCKS.asm
@@ -7,6 +7,6 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-map_blocks_L1_size_b	equ 0x00030600	; Size in bytes
+map_blocks_L1_size_b	equ 0x00030800	; Size in bytes
 map_blocks_L1_size_w	equ (map_blocks_L1_size_b/2)	; Size in words
 map_blocks_L1_size_l	equ (map_blocks_L1_size_b/4)	; Size in longwords
--- bigevilframework/TANGLEWD/ASSETS/MAPS/L1/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L1/COLBLOCK.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L1_size_b	equ 0x0000E860	; Size in bytes
+terrainmap_blocks_L1_size_b	equ 0x0000EA00	; Size in bytes
 terrainmap_blocks_L1_size_w	equ (terrainmap_blocks_L1_size_b/2)	; Size in words
 terrainmap_blocks_L1_size_l	equ (terrainmap_blocks_L1_size_b/4)	; Size in longwords
-terrainmap_blocks_L1_num_blocks	equ 1859	; Size in blocks
+terrainmap_blocks_L1_num_blocks	equ 1872	; Size in blocks
