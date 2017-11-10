a4499c6364b3eecda032a7fd553873d8648ff284
Matt <mattphillips@mail.com>
2017-11-10 18:48:29 +0000

    Tanglewood: L5A3 fixed Echo/Fuzzl puzzle trigger box

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L5/COLTILES.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L5/COLTILES.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-TerrainTiles_L5_size_b	equ 0x000004A0	; Size in bytes
+TerrainTiles_L5_size_b	equ 0x000004B0	; Size in bytes
 TerrainTiles_L5_size_w	equ (TerrainTiles_L5_size_b/2)	; Size in words
 TerrainTiles_L5_size_l	equ (TerrainTiles_L5_size_b/4)	; Size in longwords
 TerrainTiles_L5_size_t	equ (TerrainTiles_L5_size_b/32)	; Size in tiles
