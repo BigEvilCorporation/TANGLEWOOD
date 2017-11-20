02ae62d8e042110f23c50ddaeaf0f4dcaf2a2ebf
Matt <mattphillips@mail.com>
2017-11-20 18:27:07 +0000

    Tanglewood: L4A1 fixed low ceiling

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L4/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L4/COLBLOCK.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L4_size_b	equ 0x00008640	; Size in bytes
+terrainmap_blocks_L4_size_b	equ 0x00008660	; Size in bytes
 terrainmap_blocks_L4_size_w	equ (terrainmap_blocks_L4_size_b/2)	; Size in words
 terrainmap_blocks_L4_size_l	equ (terrainmap_blocks_L4_size_b/4)	; Size in longwords
-terrainmap_blocks_L4_num_blocks	equ 1074	; Size in blocks
+terrainmap_blocks_L4_num_blocks	equ 1075	; Size in blocks
