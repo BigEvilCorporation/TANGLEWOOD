d3a76c70f89ef221fee12e2e7361aa42e671c2b0
Matt <mattphillips@mail.com>
2017-12-02 23:21:06 +0000

    Tanglewood: L6 updates

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L6/BLOCKS.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L6/BLOCKS.asm
@@ -7,6 +7,6 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-map_blocks_L6_size_b	equ 0x000114E0	; Size in bytes
+map_blocks_L6_size_b	equ 0x00011720	; Size in bytes
 map_blocks_L6_size_w	equ (map_blocks_L6_size_b/2)	; Size in words
 map_blocks_L6_size_l	equ (map_blocks_L6_size_b/4)	; Size in longwords
