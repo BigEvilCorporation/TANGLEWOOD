e62d174148184d3d8a1c8c21f364ceadfa770a4a
Matt Phillips <mattphillips@mail.com>
2017-12-15 14:52:14 +0000

    Tanglewood: L4A2 improvements to water/lock puzzle

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L4/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L4/COLBLOCK.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L4_size_b	equ 0x00008980	; Size in bytes
+terrainmap_blocks_L4_size_b	equ 0x000089A0	; Size in bytes
 terrainmap_blocks_L4_size_w	equ (terrainmap_blocks_L4_size_b/2)	; Size in words
 terrainmap_blocks_L4_size_l	equ (terrainmap_blocks_L4_size_b/4)	; Size in longwords
-terrainmap_blocks_L4_num_blocks	equ 1100	; Size in blocks
+terrainmap_blocks_L4_num_blocks	equ 1101	; Size in blocks
