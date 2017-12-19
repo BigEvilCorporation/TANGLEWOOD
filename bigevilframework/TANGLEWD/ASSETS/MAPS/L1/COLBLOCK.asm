072ebcd7164e0fbe10db2f462d772bea0698d2ba
Matt Phillips <mattphillips@mail.com>
2017-12-19 17:51:58 +0000

    Tanglewood: L1A2 fixed flue bounds

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L1/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L1/COLBLOCK.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L1_size_b	equ 0x0000EAC0	; Size in bytes
+terrainmap_blocks_L1_size_b	equ 0x0000EA80	; Size in bytes
 terrainmap_blocks_L1_size_w	equ (terrainmap_blocks_L1_size_b/2)	; Size in words
 terrainmap_blocks_L1_size_l	equ (terrainmap_blocks_L1_size_b/4)	; Size in longwords
-terrainmap_blocks_L1_num_blocks	equ 1878	; Size in blocks
+terrainmap_blocks_L1_num_blocks	equ 1876	; Size in blocks
