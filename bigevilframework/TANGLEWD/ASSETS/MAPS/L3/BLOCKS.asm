e71eea9f1768f5cd03468f1c47a38be9f91ad19c
Matt Phillips <mattphillips@mail.com>
2018-02-04 19:06:05 +0000

    Tanglewood: L3 fixed black tiles

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L3/BLOCKS.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L3/BLOCKS.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-map_blocks_L3_size_b	equ 0x00024680	; Size in bytes
+map_blocks_L3_size_b	equ 0x000242C0	; Size in bytes
 map_blocks_L3_size_w	equ (map_blocks_L3_size_b/2)	; Size in words
 map_blocks_L3_size_l	equ (map_blocks_L3_size_b/4)	; Size in longwords
-map_blocks_L3_num_blocks	equ 4660	; Size in blocks
+map_blocks_L3_num_blocks	equ 4630	; Size in blocks
