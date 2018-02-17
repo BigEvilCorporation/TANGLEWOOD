6fd39fb5752d921fb0052b29bea6eba7e13b9bf2
Matt Phillips <mattphillips@mail.com>
2018-02-17 15:26:38 +0000

    Tanglewood: Fixed background tiling

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L4/BLOCKS.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L4/BLOCKS.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-map_blocks_L4_size_b	equ 0x000260C0	; Size in bytes
+map_blocks_L4_size_b	equ 0x00026220	; Size in bytes
 map_blocks_L4_size_w	equ (map_blocks_L4_size_b/2)	; Size in words
 map_blocks_L4_size_l	equ (map_blocks_L4_size_b/4)	; Size in longwords
-map_blocks_L4_num_blocks	equ 4870	; Size in blocks
+map_blocks_L4_num_blocks	equ 4881	; Size in blocks
