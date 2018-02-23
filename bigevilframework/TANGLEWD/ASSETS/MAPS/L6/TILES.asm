6324dbc8f50452a400905e175129f87e5b4e98bb
Matt Phillips <mattphillips@mail.com>
2018-02-23 19:29:24 +0000

    Tanglewood: L6 background update

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L6/TILES.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L6/TILES.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-tiles_L6_size_b	equ 0x000074C0	; Size in bytes
+tiles_L6_size_b	equ 0x000075A0	; Size in bytes
 tiles_L6_size_w	equ (tiles_L6_size_b/2)	; Size in words
 tiles_L6_size_l	equ (tiles_L6_size_b/4)	; Size in longwords
-tiles_L6_size_t	equ 934	; Size in tiles
+tiles_L6_size_t	equ 941	; Size in tiles
