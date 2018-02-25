8cb9b63b00c7d27e81019fa8082138f72b6e0c5f
Matt Phillips <mattphillips@mail.com>
2018-02-25 18:34:50 +0000

    Tanglewood: L1A3 made green Fuzzl more easily accessible

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L1/BLOCKS.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L1/BLOCKS.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-map_blocks_L1_size_b	equ 0x0002DA80	; Size in bytes
+map_blocks_L1_size_b	equ 0x0002E000	; Size in bytes
 map_blocks_L1_size_w	equ (map_blocks_L1_size_b/2)	; Size in words
 map_blocks_L1_size_l	equ (map_blocks_L1_size_b/4)	; Size in longwords
-map_blocks_L1_num_blocks	equ 5844	; Size in blocks
+map_blocks_L1_num_blocks	equ 5888	; Size in blocks
