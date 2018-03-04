e74a5076488b544210021b505c9e9adee2bcaa79
Matt Phillips <mattphillips@mail.com>
2018-03-04 13:26:21 +0000

    Tanglewood: L5A4 updates

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L4/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L4/COLBLOCK.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L4_size_b	equ 0x00009AC0	; Size in bytes
+terrainmap_blocks_L4_size_b	equ 0x00009DE0	; Size in bytes
 terrainmap_blocks_L4_size_w	equ (terrainmap_blocks_L4_size_b/2)	; Size in words
 terrainmap_blocks_L4_size_l	equ (terrainmap_blocks_L4_size_b/4)	; Size in longwords
-terrainmap_blocks_L4_num_blocks	equ 1238	; Size in blocks
+terrainmap_blocks_L4_num_blocks	equ 1263	; Size in blocks
