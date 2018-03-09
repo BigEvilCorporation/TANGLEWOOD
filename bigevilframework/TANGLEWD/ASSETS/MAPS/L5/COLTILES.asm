7520c748a4a04826e7daccb5ddeb03d2696336b0
Matt Phillips <mattphillips@mail.com>
2018-03-09 18:10:16 +0000

    Tanglewood: Scrap AI - knocks Fuzzl out of Nymn's hands if he tried to roll it

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L5/COLTILES.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L5/COLTILES.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-TerrainTiles_L5_size_b	equ 0x000004D0	; Size in bytes
+TerrainTiles_L5_size_b	equ 0x000004D8	; Size in bytes
 TerrainTiles_L5_size_w	equ (TerrainTiles_L5_size_b/2)	; Size in words
 TerrainTiles_L5_size_l	equ (TerrainTiles_L5_size_b/4)	; Size in longwords
-TerrainTiles_L5_size_t	equ 154	; Size in tiles
+TerrainTiles_L5_size_t	equ 155	; Size in tiles
