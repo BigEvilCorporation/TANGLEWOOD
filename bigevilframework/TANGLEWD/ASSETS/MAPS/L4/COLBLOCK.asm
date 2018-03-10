11e2b9a4357d4aa5c8eb6704796e3edb91cf6fbf
Matt Phillips <mattphillips@mail.com>
2018-03-10 16:42:47 +0000

    Tanglewood: L8A4 Nymn revival cutscene

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L4/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L4/COLBLOCK.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L4_size_b	equ 0x0000A960	; Size in bytes
+terrainmap_blocks_L4_size_b	equ 0x0000A880	; Size in bytes
 terrainmap_blocks_L4_size_w	equ (terrainmap_blocks_L4_size_b/2)	; Size in words
 terrainmap_blocks_L4_size_l	equ (terrainmap_blocks_L4_size_b/4)	; Size in longwords
-terrainmap_blocks_L4_num_blocks	equ 1355	; Size in blocks
+terrainmap_blocks_L4_num_blocks	equ 1348	; Size in blocks
