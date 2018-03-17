f44933222bcedfed62d275db211d052face847cf
Matt Phillips <mattphillips@mail.com>
2018-03-17 15:22:52 +0000

    Tanglewood: L7A1 fixed exploit: can bypass green Fuzzl section

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L7/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L7/COLBLOCK.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L7_size_b	equ 0x0000F3E0	; Size in bytes
+terrainmap_blocks_L7_size_b	equ 0x0000F5E0	; Size in bytes
 terrainmap_blocks_L7_size_w	equ (terrainmap_blocks_L7_size_b/2)	; Size in words
 terrainmap_blocks_L7_size_l	equ (terrainmap_blocks_L7_size_b/4)	; Size in longwords
-terrainmap_blocks_L7_num_blocks	equ 1951	; Size in blocks
+terrainmap_blocks_L7_num_blocks	equ 1967	; Size in blocks
