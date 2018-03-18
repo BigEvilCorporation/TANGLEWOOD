d61f8486ad9b8d8f5fb3871a93bd320bf341cb6f
Matt Phillips <mattphillips@mail.com>
2018-03-18 20:49:32 +0000

    Tanglewood: L5A3 added locks to raft section, and cutscene

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L5/BLOCKS.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L5/BLOCKS.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-map_blocks_L5_size_b	equ 0x00025A80	; Size in bytes
+map_blocks_L5_size_b	equ 0x000259C0	; Size in bytes
 map_blocks_L5_size_w	equ (map_blocks_L5_size_b/2)	; Size in words
 map_blocks_L5_size_l	equ (map_blocks_L5_size_b/4)	; Size in longwords
-map_blocks_L5_num_blocks	equ 4820	; Size in blocks
+map_blocks_L5_num_blocks	equ 4814	; Size in blocks
