853cd0022817418f9fc4c41a9c0722d6aca29a0f
Matt Phillips <mattphillips@mail.com>
2018-03-23 17:35:21 +0000

    Tanglewood: L8A1 fixed missing stamps

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L7/BLOCKS.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L7/BLOCKS.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-map_blocks_L7_size_b	equ 0x00018BE0	; Size in bytes
+map_blocks_L7_size_b	equ 0x00018E20	; Size in bytes
 map_blocks_L7_size_w	equ (map_blocks_L7_size_b/2)	; Size in words
 map_blocks_L7_size_l	equ (map_blocks_L7_size_b/4)	; Size in longwords
-map_blocks_L7_num_blocks	equ 3167	; Size in blocks
+map_blocks_L7_num_blocks	equ 3185	; Size in blocks
--- bigevilframework/TANGLEWD/ASSETS/MAPS/L7/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L7/COLBLOCK.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L7_size_b	equ 0x0000F5A0	; Size in bytes
+terrainmap_blocks_L7_size_b	equ 0x0000F580	; Size in bytes
 terrainmap_blocks_L7_size_w	equ (terrainmap_blocks_L7_size_b/2)	; Size in words
 terrainmap_blocks_L7_size_l	equ (terrainmap_blocks_L7_size_b/4)	; Size in longwords
-terrainmap_blocks_L7_num_blocks	equ 1965	; Size in blocks
+terrainmap_blocks_L7_num_blocks	equ 1964	; Size in blocks
