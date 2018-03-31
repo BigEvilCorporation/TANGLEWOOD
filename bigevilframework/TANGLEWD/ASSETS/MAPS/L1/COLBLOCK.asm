53d47b28cf00ed923c61672d4573dd5088260d6a
Matt Phillips <mattphillips@mail.com>
2018-03-31 20:53:57 +0100

    Tanglewood: L1A3 fixed missing wall

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L1/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L1/COLBLOCK.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L1_size_b	equ 0x0000EDE0	; Size in bytes
+terrainmap_blocks_L1_size_b	equ 0x0000EDA0	; Size in bytes
 terrainmap_blocks_L1_size_w	equ (terrainmap_blocks_L1_size_b/2)	; Size in words
 terrainmap_blocks_L1_size_l	equ (terrainmap_blocks_L1_size_b/4)	; Size in longwords
-terrainmap_blocks_L1_num_blocks	equ 1903	; Size in blocks
+terrainmap_blocks_L1_num_blocks	equ 1901	; Size in blocks
