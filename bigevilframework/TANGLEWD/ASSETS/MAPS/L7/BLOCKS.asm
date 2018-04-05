1366300befbac4085f3af14a6ed0bd72a8b0a4f4
Matt Phillips <mattphillips@mail.com>
2018-04-05 01:10:41 +0100

    Tanglewood: L7 flue mask stamp pass

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L7/BLOCKS.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L7/BLOCKS.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-map_blocks_L7_size_b	equ 0x00018C60	; Size in bytes
+map_blocks_L7_size_b	equ 0x00018DA0	; Size in bytes
 map_blocks_L7_size_w	equ (map_blocks_L7_size_b/2)	; Size in words
 map_blocks_L7_size_l	equ (map_blocks_L7_size_b/4)	; Size in longwords
-map_blocks_L7_num_blocks	equ 3171	; Size in blocks
+map_blocks_L7_num_blocks	equ 3181	; Size in blocks
