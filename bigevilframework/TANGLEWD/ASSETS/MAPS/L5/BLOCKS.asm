31aea445700ab2f132b0e83fdb5d0f30dd0fb4d1
Matt Phillips <mattphillips@mail.com>
2018-04-11 10:38:14 +0100

    Tanglewood: L5A2 fixed jump/glide section

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L5/BLOCKS.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L5/BLOCKS.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-map_blocks_L5_size_b	equ 0x00026280	; Size in bytes
+map_blocks_L5_size_b	equ 0x00026320	; Size in bytes
 map_blocks_L5_size_w	equ (map_blocks_L5_size_b/2)	; Size in words
 map_blocks_L5_size_l	equ (map_blocks_L5_size_b/4)	; Size in longwords
-map_blocks_L5_num_blocks	equ 4884	; Size in blocks
+map_blocks_L5_num_blocks	equ 4889	; Size in blocks
--- bigevilframework/TANGLEWD/ASSETS/MAPS/L5/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L5/COLBLOCK.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L5_size_b	equ 0x00005860	; Size in bytes
+terrainmap_blocks_L5_size_b	equ 0x000058E0	; Size in bytes
 terrainmap_blocks_L5_size_w	equ (terrainmap_blocks_L5_size_b/2)	; Size in words
 terrainmap_blocks_L5_size_l	equ (terrainmap_blocks_L5_size_b/4)	; Size in longwords
-terrainmap_blocks_L5_num_blocks	equ 707	; Size in blocks
+terrainmap_blocks_L5_num_blocks	equ 711	; Size in blocks
