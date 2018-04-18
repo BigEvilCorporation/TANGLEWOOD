6f895d8557ec56c1656d857fd735588268c29523
Matt Phillips <mattphillips@mail.com>
2018-04-18 22:03:52 +0100

    Tanglewood: L6A1 allow more room before running into Djakk, don't block player control

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L6/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L6/COLBLOCK.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L6_size_b	equ 0x000040E0	; Size in bytes
+terrainmap_blocks_L6_size_b	equ 0x000041A0	; Size in bytes
 terrainmap_blocks_L6_size_w	equ (terrainmap_blocks_L6_size_b/2)	; Size in words
 terrainmap_blocks_L6_size_l	equ (terrainmap_blocks_L6_size_b/4)	; Size in longwords
-terrainmap_blocks_L6_num_blocks	equ 519	; Size in blocks
+terrainmap_blocks_L6_num_blocks	equ 525	; Size in blocks
