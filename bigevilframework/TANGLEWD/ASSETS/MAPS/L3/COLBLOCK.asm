4779b0e80b5b28a6780214d3c9da2403eb08e864
Matt Phillips <mattphillips@mail.com>
2018-04-29 18:53:03 +0100

    Tanglewood: L3A3 fixed wall above Elder cage

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L3/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L3/COLBLOCK.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L3_size_b	equ 0x000048E0	; Size in bytes
+terrainmap_blocks_L3_size_b	equ 0x00004920	; Size in bytes
 terrainmap_blocks_L3_size_w	equ (terrainmap_blocks_L3_size_b/2)	; Size in words
 terrainmap_blocks_L3_size_l	equ (terrainmap_blocks_L3_size_b/4)	; Size in longwords
-terrainmap_blocks_L3_num_blocks	equ 583	; Size in blocks
+terrainmap_blocks_L3_num_blocks	equ 585	; Size in blocks
