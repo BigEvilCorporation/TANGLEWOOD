9a5a12c94d3f392d0b73ba64d08cf357ea2409e7
Matt Phillips <mattphillips@mail.com>
2018-05-01 21:32:24 +0100

    Tanglewood: L5 replaced curved corner stamp with 90 degree edge to prevent player jumping through terrain

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L5/BLOCKS.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L5/BLOCKS.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-map_blocks_L5_size_b	equ 0x00026300	; Size in bytes
+map_blocks_L5_size_b	equ 0x000263C0	; Size in bytes
 map_blocks_L5_size_w	equ (map_blocks_L5_size_b/2)	; Size in words
 map_blocks_L5_size_l	equ (map_blocks_L5_size_b/4)	; Size in longwords
-map_blocks_L5_num_blocks	equ 4888	; Size in blocks
+map_blocks_L5_num_blocks	equ 4894	; Size in blocks
