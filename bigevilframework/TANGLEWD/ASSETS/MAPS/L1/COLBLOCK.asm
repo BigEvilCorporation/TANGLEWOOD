51d60ef6931d595fc7ef39857455d956eedd1af2
Matt Phillips <mattphillips@mail.com>
2018-05-03 21:41:49 +0100

    Tanglewood: L1A2 raised ceiling in cave tunnel to allow player to stand on top of boulder without clipping through collision

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L1/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L1/COLBLOCK.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L1_size_b	equ 0x0000F400	; Size in bytes
+terrainmap_blocks_L1_size_b	equ 0x0000F3E0	; Size in bytes
 terrainmap_blocks_L1_size_w	equ (terrainmap_blocks_L1_size_b/2)	; Size in words
 terrainmap_blocks_L1_size_l	equ (terrainmap_blocks_L1_size_b/4)	; Size in longwords
-terrainmap_blocks_L1_num_blocks	equ 1952	; Size in blocks
+terrainmap_blocks_L1_num_blocks	equ 1951	; Size in blocks
