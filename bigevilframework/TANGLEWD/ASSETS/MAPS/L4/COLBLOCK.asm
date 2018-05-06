2fdbd729d70a2b35ef6523e3bb02df9c07035db3
Matt Phillips <mattphillips@mail.com>
2018-05-06 17:01:37 +0100

    Tanglewood: L4A1 Changed last bridge to roll down instead of up (barrier behaviour is glitchy), adjusted level layout to suit

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L4/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L4/COLBLOCK.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L4_size_b	equ 0x0000AAC0	; Size in bytes
+terrainmap_blocks_L4_size_b	equ 0x0000AAE0	; Size in bytes
 terrainmap_blocks_L4_size_w	equ (terrainmap_blocks_L4_size_b/2)	; Size in words
 terrainmap_blocks_L4_size_l	equ (terrainmap_blocks_L4_size_b/4)	; Size in longwords
-terrainmap_blocks_L4_num_blocks	equ 1366	; Size in blocks
+terrainmap_blocks_L4_num_blocks	equ 1367	; Size in blocks
