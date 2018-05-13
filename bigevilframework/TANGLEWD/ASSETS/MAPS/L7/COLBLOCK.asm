734724f6b020b2102407040c4bc96b670fc1cf8d
Matt Phillips <mattphillips@mail.com>
2018-05-13 18:29:16 +0100

    Tanglewood: Fixed terrain in abandoned level, just in case someone finds it

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L7/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L7/COLBLOCK.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L7_size_b	equ 0x0000F940	; Size in bytes
+terrainmap_blocks_L7_size_b	equ 0x0000F980	; Size in bytes
 terrainmap_blocks_L7_size_w	equ (terrainmap_blocks_L7_size_b/2)	; Size in words
 terrainmap_blocks_L7_size_l	equ (terrainmap_blocks_L7_size_b/4)	; Size in longwords
-terrainmap_blocks_L7_num_blocks	equ 1994	; Size in blocks
+terrainmap_blocks_L7_num_blocks	equ 1996	; Size in blocks
