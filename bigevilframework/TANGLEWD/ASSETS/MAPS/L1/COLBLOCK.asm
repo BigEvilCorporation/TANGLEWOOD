fb1b0c83e4bd38e8475e9cb75a66ea2d306ec117
DESKTOP-99MT4GR\Matt <matt@tanglewoodgame.com>
2018-05-17 20:20:59 +0100

    Tanglewood: L1A2 Fixed hole in terrain

--- bigevilframework/TANGLEWD/ASSETS/MAPS/L1/COLBLOCK.asm
+++ bigevilframework/TANGLEWD/ASSETS/MAPS/L1/COLBLOCK.asm
@@ -7,7 +7,7 @@
 ; == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 
-terrainmap_blocks_L1_size_b	equ 0x0000D960	; Size in bytes
+terrainmap_blocks_L1_size_b	equ 0x0000D900	; Size in bytes
 terrainmap_blocks_L1_size_w	equ (terrainmap_blocks_L1_size_b/2)	; Size in words
 terrainmap_blocks_L1_size_l	equ (terrainmap_blocks_L1_size_b/4)	; Size in longwords
-terrainmap_blocks_L1_num_blocks	equ 1739	; Size in blocks
+terrainmap_blocks_L1_num_blocks	equ 1736	; Size in blocks
